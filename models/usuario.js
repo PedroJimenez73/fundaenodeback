6b6936c3d4ea464a0f9ce4fcd8e253fe6349bfcc
pedroisbanjunio19 <pedroisbanjunio19.gmail.com>
2019-07-08 21:52:36 +0200

    login 3

--- models/usuario.js
+++ models/usuario.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
-var unique = require('mongoose-unique-validator');
+// var unique = require('mongoose-unique-validator');
 
 var UsuarioSchema = new mongoose.Schema({
     nombre: String,
-    email: {type: String, unique: true},
+    // email: {type: String, unique: true},
+    email: String,
     password: String,
     rol: String,
 });
 
-UsuarioSchema.plugin(unique, {message: 'Nombre de usuario ya se encuentra en uso'});
+// UsuarioSchema.plugin(unique, {message: 'Nombre de usuario ya se encuentra en uso'});
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
