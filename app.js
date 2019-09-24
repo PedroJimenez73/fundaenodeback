8cabe9bdbdf344e68b9a8b4d732456360dc1594e
PedroJimenez73 <pjimenez@qualitycw.com>
2019-09-24 18:09:54 +0200

    imagenes4

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', false);
     next();
   });
 
