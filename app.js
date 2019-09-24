0fd91621ba08ad22d674056e7196b834368ee5ee
PedroJimenez73 <pjimenez@qualitycw.com>
2019-09-24 19:32:16 +0200

    imagenes6

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ var conn = mongoose.connection;
 let gfs;
 
 app.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
 
